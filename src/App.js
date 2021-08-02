9a3e6a29bd1f099b6cc2e7342f40e665ffab22cd
SangeethaShanmugm <83452706+SangeethaShanmugm@users.noreply.github.com>
2021-08-02 19:01:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Card from "./Card";
+
+export default function App() {
+  return (
+    <>
+      <section class="pricing py-5">
+        <div class="container">
+          <Card />;
+        </div>
+      </section>
+    </>
+  );
+}
